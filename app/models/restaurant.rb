16cceacbbfd8ff3f39ca922f51bf1d46bc0ca4e6
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2019-11-13 11:54:16 +0100

    Add restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  SPECIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: { in: SPECIES }
 end
