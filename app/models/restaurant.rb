e144b8e2f35f586e9d76da73a23b369cda766fe0
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2019-11-13 17:25:24 +0100

    Ex en cours

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
-  SPECIES = ["chinese", "italian", "japanese", "french", "belgian"]
-  validates :category, inclusion: { in: SPECIES }
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: { in: CATEGORIES }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create(name: "La tour d'argent", address: "Paris", phone_number: "01.90.81.18.22", category: "french")
+Restaurant.create(name: "Pizza nuevo", address: "Bordeaux", phone_number: "01.90.81.18.58", category: "italian")
+Restaurant.create(name: "Belgium fries", address: "Bruges", phone_number: "09.90.81.42.58", category: "belgian")
+Restaurant.create(name: "Sushi shop", address: "Montpellier", phone_number: "07.90.81.42.58", category: "japanese")
+Restaurant.create(name: "ramen", address: "Bordeaux", phone_number: "08.90.81.42.58", category: "chinese")
